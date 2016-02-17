8d9ca3c7c36cfa5fa0f01583b7b85707e9524878
Michael Slone <m.slone@gmail.com>
2016-02-17 10:27:01 -0500

    initial import

---
+++ lib/SAP/InboundInterface.pm
@@ -0,0 +1,323 @@
+package SAP::InboundInterface;
+
+use 5.010;
+use strict;
+use warnings;
+
+require Exporter;
+
+our @ISA = qw(Exporter);
+
+our %EXPORT_TAGS = ( 'all' => [ qw(
+    initialize_invoice
+    set_header
+    add_invoice_line
+    render_invoice
+    normalize
+) ] );
+
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+our @EXPORT = ();
+our $VERSION = '0.01';
+
+=pod
+
+=head1 NAME
+
+SAP::InboundInterface - SAP invoice data file generator
+
+=head1 SYNOPSIS
+
+    initialize_invoice();
+    set_header({
+        'SGTXT' => 'Header',
+        'DOCDATE' => '20160208',
+        'BASELINEDATE' => '20160214',
+        'AMOUNT' => '12.98',
+    });
+    add_invoice_line({
+        'AMOUNT' => '12.00',
+        'SGTXT' => 'subtotal',
+    });
+    add_invoice_line({
+        'AMOUNT' => '0.98',
+        'SGTXT' => 'tax',
+    });
+    print render_invoice();
+
+=head1 DESCRIPTION
+
+SAP::InboundInterface is a module to simplify generation of SAP invoice data 
+files.  It does not actually create the data files; that is the responsibility
+of the caller.  Instead, it writes to C<STDOUT>.
+
+=head2 Header columns
+
+The following header columns are supported.  For length, alignment, and
+default value, consult C<InboundInterface.pm>.
+
+=over 4
+
+=item TYPE
+
+=item SGTXT
+
+=item DOCDATE
+
+=item BASELINEDATE
+
+=item XBLNR
+
+=item VENDORTYPE
+
+=item LIFNR
+
+=item AMOUNT
+
+=item BUSCS
+
+=item ZLSCH
+
+=item UZAWE
+
+=item ZLSPR
+
+=item REGUL
+
+=back
+
+=cut
+
+# For each column, specify the length, preferred alignment, and default value.
+# Fill character is space.
+my @header_format = (
+    # Column        Len Align Default
+    ['TYPE',          1, 'l', 'H'   ],
+    ['SGTXT',        50, 'l', ''    ],
+    ['DOCDATE',       8, 'l', ''    ],
+    ['BASELINEDATE',  8, 'l', ''    ],
+    ['XBLNR',        16, 'l', ''    ],
+    ['VENDORTYPE',    4, 'l', 'ZOTV'],
+    ['LIFNR',        10, 'l', ''    ],
+    ['AMOUNT',       16, 'r', ''    ],
+    ['BUSCS',         1, 'l', 'R'   ],
+    ['ZLSCH',         1, 'l', ''    ],
+    ['UZAWE',         2, 'l', ''    ],
+    ['ZLSPR',         1, 'l', ''    ],
+    ['REGUL',         1, 'l', ''    ],
+);
+my @header_columns = ();
+my %header_format = ();
+foreach my $header_format (@header_format) {
+    my $column = shift @{$header_format};
+    push @header_columns, $column;
+    $header_format{$column} = $header_format;
+}
+
+=pod 
+
+=head2 Invoice line columns
+
+The following invoice line columns are supported.  For length, alignment, and
+default value, consult C<InboundInterface.pm>.
+
+=over 4
+
+=item TYPE
+
+=item SAKNR
+
+=item KOSTL
+
+=item AMOUNT
+
+=item SHKZG
+
+=item SGTXT
+
+=back
+
+=cut
+
+my @detail_format = (
+    # Column  Len Align Default
+    ['TYPE',    1, 'l', 'D'],
+    ['SAKNR',  10, 'l', '' ],
+    ['KOSTL',  10, 'l', '' ],
+    ['AMOUNT', 16, 'r', '' ],
+    ['SHKZG',   1, 'l', 'S'],
+    ['SGTXT',  50, 'l', '' ],
+);
+my @detail_columns = ();
+my %detail_format = ();
+foreach my $detail_format (@detail_format) {
+    my $column = shift @{$detail_format};
+    push @detail_columns, $column;
+    $detail_format{$column} = $detail_format;
+}
+
+my %header = ();
+my @details = ();
+
+=pod
+
+=head2 Methods
+
+=over 4
+
+=item C<initialize_invoice()>
+
+Reset the header to the default, and clear any existing invoice lines.
+
+=cut
+
+sub initialize_invoice {
+    %header = ();
+    foreach my $column (@header_columns) {
+        my $default = ${$header_format{$column}}[2];
+        $header{$column} = $default;
+    }
+    @details = ();
+}
+
+=pod
+
+=item C<set_header({'column' =E<gt> $value, ...})>
+
+Set the given columns to the specified values.  Omitted columns are ignored.
+Thus this method could be called several times with partial information if
+desired, or once with a single hash reference.
+
+=cut
+
+sub set_header {
+    my (
+        $hash_ref,
+    ) = @_;
+    foreach my $column (keys %{$hash_ref}) {
+        $header{$column} = ${$hash_ref}{$column};
+    }
+}
+
+=pod
+
+=item C<add_invoice_line({'column' =E<gt> $value, ...})>
+
+Initialize a fresh invoice line with the default data, set the given columns
+to the specified values, and append the result to the current invoice.
+
+=cut
+
+sub add_invoice_line {
+    my (
+        $hash_ref,
+    ) = @_;
+
+    # We shouldn't get called after render_invoice(), but if we are,
+    # we need to make sure that there's only one invoice line marked with
+    # TYPE 'L'.
+    if (scalar(@details) > 0) {
+        ${$details[$#details]}{'TYPE'} = 'D';
+    }
+
+    # Initialize a new detail line.
+    my %detail = ();
+    foreach my $column (@detail_columns) {
+        my $default = ${$detail_format{$column}}[2];
+        $detail{$column} = $default;
+    }
+
+    # Store the user-requested columns.
+    foreach my $column (keys %{$hash_ref}) {
+        $detail{$column} = ${$hash_ref}{$column};
+    }
+
+    # Add to the current invoice.
+    push @details, \%detail;
+}
+
+=pod
+
+=item C<render_invoice()>
+
+Format the current invoice and print it to STDOUT.
+
+=cut
+
+sub render_invoice {
+    # Ensure that final detail is labeled properly.
+    ${$details[$#details]}{'TYPE'} = 'L';
+
+    my @output = ();
+    push @output, render(\%header, \%header_format, @header_columns);
+    foreach my $detail_ref (@details) {
+        push @output, render($detail_ref, \%detail_format, @detail_columns);
+    }
+
+    return join('', @output);
+}
+
+sub render {
+    my (
+        $hash_ref,
+        $format_ref,
+        @columns,
+    ) = @_;
+
+    my @pieces = ();
+    foreach my $column (@columns) {
+        my $normalized = normalize($format_ref, $column, ${$hash_ref}{$column});
+        push @pieces, $normalized;
+    }
+
+    return join('', @pieces) . "\r\n";
+}
+
+sub normalize {
+    my (
+        $format_ref,
+        $column,
+        $value,
+    ) = @_;
+
+    if (exists ${$format_ref}{$column}) {
+        my $format    = ${$format_ref}{$column};
+        my $size      = ${$format}[0];
+        my $alignment = ${$format}[1];
+
+        if (length($value) > $size) {
+            $value = substr($value, 0, $size);
+        }
+
+        if (length($value) < $size) {
+            my $spacer = ' ' x ($size - length($value));
+            if ('r' eq $alignment) {
+                $value = $spacer . $value;
+            }
+            else {
+                $value = $value . $spacer;
+            }
+        }
+
+        return $value;
+    }
+    else {
+        return 0;
+    }
+}
+
+=pod
+
+=back
+
+=head1 COPYRIGHT
+
+Copyright (C) 2016 Michael Slone <m.slone@gmail.com>.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.0 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
+
+1;
---
+++ reader.pl
@@ -0,0 +1,293 @@
+#!/usr/bin/env perl -w
+use strict;
+use warnings;
+use Fcntl ':flock';
+use Getopt::Long;
+use HTML::Entities;
+use POSIX;
+use XML::LibXML;
+use SAP::InboundInterface ':all';
+use feature 'say';
+
+# The inbox and todo directory handling requires that
+# only one copy of the reader is running at a time.
+INIT {
+    open *{0}
+        or die "$0: failed: $!";
+    flock *{0}, LOCK_EX|LOCK_NB
+        or die "$0 is already running\n";
+}
+
+# Pick up configuration.
+my $root = 0;
+my $destination = 0;
+my $log = 0;
+# It would be nice to check against this.
+#my $xsd = 'https://developers.exlibrisgroup.com/resources/xsd/invoice_payment.xsd';
+
+GetOptions(
+    'root=s'        => \$root,
+    'destination=s' => \$destination,
+    'log=s'         => \$log)
+    or die "$0: can't load options: $!";
+
+if (!$root || !$destination) {
+    if ($log) {
+        debug("no configuration available, exiting");
+    }
+    die "$0: no configuration available, exiting";
+}
+
+my $inbox   = "$root/inbox";
+my $todo    = "$root/todo";
+my $outbox  = "$root/outbox";
+my $success = "$root/success";
+my $failure = "$root/failure";
+
+debug("queueing Alma XML files for processing");
+my $dh;
+opendir ($dh, $inbox)
+    or die "$0: can't open directory $inbox: $!";
+foreach my $file (readdir $dh) {
+    next if $file =~ /^\./;
+    if ($file =~ /\.xml$/ and -f "$inbox/$file") {
+        rename "$inbox/$file", "$todo/$file";
+    }
+}
+
+# Begin building data file
+# Sample data file name:  d_aplibr_uklibraries_2015-10-15-210001
+my $data_file = strftime('d_aplibr_uklibraries_%Y-%m-%d-%H%M%S', localtime());
+debug("Generating data file $data_file in $outbox");
+
+# Process Alma XML
+opendir ($dh, $todo)
+    or die "$0: can't open directory $todo: $!";
+open(my $data_fh, '>', "$outbox/$data_file")
+    or die "$0: can't open file $outbox/$data_file for output: $!";
+foreach my $file (readdir $dh) {
+    next if $file =~ /^\./;
+    if ($file =~ /\.xml$/ and -f "$todo/$file") {
+        my ($error, @entries) = export_invoices("$todo/$file");
+        if ($error) {
+            debug("failed to generate output for $file");
+            rename "$todo/$file", "$failure/$file";
+        }
+        else {
+            print $data_fh join('', @entries);
+            rename "$todo/$file", "$success/$file";
+        }
+    }
+}
+close($data_fh) or die "$0: can't close file $outbox/$data_file: $!";
+
+debug("Submitting $data_file to $destination");
+rename "$outbox/$data_file", "$destination/inbox/$data_file";
+
+sub export_invoices {
+    my (
+        $alma_file,
+        $success,
+        $failure,
+    ) = @_;
+
+    my @export = ();
+
+    debug("processing $alma_file");
+    my $parser = XML::LibXML->new;
+    my $doc = $parser->parse_file($alma_file);
+    if (!$doc) {
+        debug("can't parse $alma_file");
+        return 1, @export;
+    }
+    my $root = $doc->documentElement;
+
+    INVOICE: foreach my $invoice ($root->getElementsByTagName('invoice')) {
+        initialize_invoice();
+
+        my %header = (
+            'SGTXT' => 'xxxxxx Univ of Kentucky Libraries',
+            'DOCDATE' => 'YYYYMMDD',
+        );
+
+        # Each invoice must contain exactly one invoice_date.
+        # The sample XML file provided by Ex Libris formats the
+        # invoice date in UK format (DDMMYYYY) instead of the
+        # ISO 8601 format required by SAP.
+        #
+        # It looks like the production export is going to use
+        # non-standard American dates (MM/DD/YYYY), so I'll check
+        # for that first, and fall back to a generic attempt to
+        # guess the format afterward.
+        my $raw_invoice_date = get_unique_field($invoice, 'invoice_date');
+        if ($raw_invoice_date =~ /^(?<month>\d\d)\/(?<day>\d\d)\/(?<year>\d\d\d\d)$/) {
+            $header{'BASELINEDATE'} = join('', $+{year}, $+{month}, $+{day});
+        }
+        else {
+            $header{'BASELINEDATE'} = convert_to_iso8601($raw_invoice_date);
+        }
+
+        $header{'XBLNR'} = get_unique_field($invoice, 'invoice_number');
+
+        # The vendor_FinancialSys_Code is optional, and the sample
+        # file from Ex Libris does not include it.
+        my @codes = $invoice->getElementsByTagName('vendor_FinancialSys_Code');
+        if (scalar(@codes) > 0) {
+            $header{'LIFNR'} = $codes[0]->getFirstChild->getData;
+        }
+
+        # We only need to process invoices with a nonzero amount.
+        my @amounts = $invoice->getElementsByTagName('invoice_amount');
+        my $amount = get_unique_field($amounts[0], 'sum');
+        if ($amount =~ /^0*\.?0*$/) {
+            next INVOICE;
+        }
+        else {
+            $header{'AMOUNT'} = $amount;
+        }
+
+        set_header(\%header);
+
+        my @invoice_lines = $invoice->getElementsByTagName('invoice_line');
+        my @valid_invoice_lines = ();
+        my $pos = 0;
+
+        INVOICE_LINE: foreach my $invoice_line (@invoice_lines) {
+            my $line_amount = get_unique_field($invoice_line, 'total_price');
+            if ($line_amount =~ /^0*\.?0*$/) {
+                next INVOICE_LINE;
+            }
+            else {
+                push @valid_invoice_lines, $invoice_line;
+            }
+        }
+
+        foreach my $invoice_line (@valid_invoice_lines) {
+            $pos++;
+            my %details = (
+                'SAKNR' => '$glcode',
+                'AMOUNT' => '',
+                'SGTXT' => '',
+            );
+
+            $details{'AMOUNT'} = get_unique_field($invoice_line, 'total_price');
+
+            # The fund_info element is repeatable.  For now, I'm just taking
+            # the first element.
+            #
+            # The external_id element is optional, and the Ex Libris sample
+            # does not include it.
+            my @external_ids = $invoice->getElementsByTagName('external_id');
+            if (scalar(@external_ids) > 0) {
+                $details{'SAKNR'} = $external_ids[0]->getFirstChild->getData;
+            }
+
+            # The po_line_info element is optional according to the spec.
+            my @po_line_infos = $invoice_line->getElementsByTagName('po_line_info');
+            if (scalar(@po_line_infos) > 0) {
+                my @sgtxt_pieces = ();
+                my $po_line_info = $po_line_infos[0];
+                my $line_number = substr(get_unique_field($po_line_info, 'po_line_number'), 0, 10);
+                # The mms_record_id element is optional, and the Ex Libris example
+                # does not use it.
+                my @mms_record_ids = $po_line_info->getElementsByTagName('mms_record_id');
+                my $bib_id = '$bibid';
+                if (scalar(@mms_record_ids) > 0) {
+                    $bib_id = get_unique_field($po_line_info, 'mms_record_id');
+                }
+                $bib_id = substr($bib_id, 0, 10);
+
+                my @titles = $po_line_info->getElementsByTagName('po_line_title');
+                my $title = ' "';
+                if (scalar(@titles) > 0) {
+                    $title = decode_entities($titles[0]->getFirstChild->getData);
+                }
+
+                $details{'SGTXT'} = sprintf("%-10s%-10s%-30s", $line_number, $bib_id, $title);
+            }
+
+            add_invoice_line(\%details);
+        }
+
+        push @export, render_invoice();
+    }
+
+    return 0, @export;
+}
+
+sub get_unique_field
+{
+    my (
+        $xml,
+        $xpath,
+    ) = @_;
+
+    my @collection = $xml->getElementsByTagName($xpath);
+    return $collection[0]->getFirstChild->getData;
+}
+
+sub convert_to_iso8601
+{
+    my (
+        $date,
+    ) = @_;
+
+    if ($date =~ /^(?<century>\d\d)(?<decade>\d\d)(?<month>\d\d)(?<day>\d\d)$/) {
+        my $century = $+{century};
+        my $decade  = $+{decade};
+        my $month   = $+{month};
+        my $day     = $+{day};
+
+        if (($month == 19 or $month == 20) and (0 <= $day and $day <= 99)) {
+            if ((1 <= $century and $century <= 31) and (1 <= $decade and $decade <= 12)) {
+                # Probably UK format.
+                ($century, $decade, $month, $day) = ($month, $day, $decade, $century);
+            }
+            elsif ((1 <= $century and $century <= 12) and (1 <= $decade and $decade <= 31)) {
+                # Probably US format.
+                ($century, $decade, $month, $day) = ($month, $day, $century, $decade);
+            }
+        }
+
+        return join('', $century, $decade, $month, $day);
+    }
+    else {
+        return '18790314';
+    }
+}
+
+sub uk_to_iso8601
+{
+    my (
+        $uk_date,
+    ) = @_;
+
+    if ($uk_date =~ /^(\d\d)(\d\d)(\d\d\d\d)$/) {
+        my $day   = $1;
+        my $month = $2;
+        my $year  = $3;
+        return join('', $year, $month, $day);
+    }
+    else {
+        return '18790314';
+    }
+}
+
+sub debug {
+    my (
+        $message,
+    ) = @_;
+
+    open(my $log_fh, '>>', $log)
+        or die "$0: can't open $log for appending: $!";
+
+    my $datestring = strftime('[%Y-%m-%d %H:%M:%S %z]:', localtime());
+
+    my @log_pieces = (
+        'Reader',
+        $datestring,
+        $message,
+    );
+
+    say $log_fh join(' ', @log_pieces);
+}
